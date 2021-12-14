0e9d2eb172ff33f5acd663d541204335fb360f41
Raul Borbei <borbeiraul@yahoo.com>
2021-12-14 18:21:11 +0200

    upgraded to version 1.1.10 with leftnavbar

--- users/views.py
+++ users/views.py
@@ -185,12 +185,12 @@ def edit_profile(request):
     if not request.user.is_authenticated:
         return redirect('index')
     
-    # photo = request.POST["inputPhoto"]
-    username = request.POST["inputUsername"]
-    email = request.POST["inputEmail"]
-    phone = request.POST["inputPhone"]
-    country = request.POST["inputCountry"]
-    address = request.POST["inputAddress"]
+    photo = request.FILES.get('inputPhoto', None)
+    username = request.POST['inputUsername']
+    email = request.POST['inputEmail']
+    phone = request.POST['inputPhone']
+    country = request.POST['inputCountry']
+    address = request.POST['inputAddress']
     
     user = request.user
     user.username = username
@@ -198,7 +198,7 @@ def edit_profile(request):
     user.save()
     
     person = Person.objects.get(user = request.user)
-    # person.photo = photo if photo != '' else person.photo
+    person.photo = photo if photo != None else person.photo
     person.phone = phone
     person.address = address
     person.country = country
