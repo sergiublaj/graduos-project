19dbadc63d9b71a81d42f68a913b3df372ea5053
Raul Borbei <borbeiraul@yahoo.com>
2021-12-14 17:26:59 +0200

    added leftnavbar in dasboard page

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,9 @@
 function dropdownMenu(){
-   var dropDown = document.getElementById("dropdownClick");
-   if(dropDown.className === "topnav"){
-      dropDown.className += " responsive";
+   var dropDown = document.getElementById('dropdownClick');
+   if(dropDown.className === 'topnav'){
+      dropDown.className += ' responsive';
    } else{
-      dropDown.className = "topnav";
+      dropDown.className = 'topnav';
    }
 }
 
@@ -14,4 +14,36 @@ setTimeout(() => {
    $('#message').fadeOut('slow');
 }, 3000);
 
-$('.toast').toast('show');
+// $('.toast').toast('show');
+
+$("#menu-toggle").click(function(e) {
+   e.preventDefault();
+   $("#wrapper").toggleClass("toggled");
+});
+$("#menu-toggle-2").click(function(e) {
+   e.preventDefault();
+   $("#wrapper").toggleClass("toggled-2");
+   $('#menu ul').hide();
+});
+
+function initMenu() {
+   $('#menu ul').hide();
+   $('#menu ul').children('.current').parent().show();
+   //$('#menu ul:first').show();
+   $('#menu li a').click(
+      function() {
+         var checkElement = $(this).next();
+         if ((checkElement.is('ul')) && (checkElement.is(':visible'))) {
+            return false;
+         }
+         if ((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
+            $('#menu ul:visible').slideUp('normal');
+            checkElement.slideDown('normal');
+            return false;
+         }
+      }
+   );
+}
+$(document).ready(function() {
+   initMenu();
+});
