c7cba5e8e571f19b99f37db7489f8f60338c52cc
Ana-Maria <anamaria_cusco@yahoo.com>
2022-01-01 16:03:07 +0200

    take version 1.3.2

--- static/js/main.js
+++ static/js/main.js
@@ -17,3 +17,31 @@ setTimeout(() => {
 // $('.toast').toast('show');
 
 $('#assignment_due').min = date.getFullYear() + "-" +  parseInt(date.getMonth() + 1 ) + "-" + date.getDate();
+
+$.fn.jQuerySimpleCounter = function( options ) {
+   var settings = $.extend({
+       start:  0,
+       end:    100,
+       easing: 'swing',
+       duration: 400,
+       complete: ''
+   }, options );
+
+   var thisElement = $(this);
+
+   $({count: settings.start}).animate({count: settings.end}, {
+     duration: settings.duration,
+     easing: settings.easing,
+     step: function() {
+        var mathCount = Math.ceil(this.count);
+        thisElement.text(mathCount);
+     },
+     complete: settings.complete
+  });
+};
+
+
+$('#number1').jQuerySimpleCounter({end: 12,duration: 3000});
+$('#number2').jQuerySimpleCounter({end: 55,duration: 3000});
+$('#number3').jQuerySimpleCounter({end: 359,duration: 2000});
+$('#number4').jQuerySimpleCounter({end: 246,duration: 2500});
