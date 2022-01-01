c3f451cba6e212cecbd61085a8bbdb9db95ae1eb
sergiumr <s3rgiumr@gmail.com>
2022-01-01 17:05:10 +0200

    added builder, factory and chain-of-responsabilty dp for registration

---
+++ users/user_handler.py
@@ -0,0 +1,66 @@
+from django.contrib import messages
+from django.shortcuts import render
+
+
+class Handler:
+    def __init__(self):
+        pass
+
+    def handle(self):
+        pass
+
+
+class UserHandler(Handler):
+    def __init__(self):
+        super(UserHandler, self).__init__()
+        self.handlers = []
+
+    def handle(self):
+        for handler in self.handlers:
+            handler.handle()
+
+    def add_handler(self, handler):
+        self.handlers.append(handler)
+
+
+class UsernameHandler(Handler):
+    def __init__(self, username, all_users, request):
+        super(UsernameHandler, self).__init__()
+        self.username = username
+        self.all_users = all_users
+        self.request = request
+
+    def handle(self):
+        if self.all_users.filter(username=self.username).exists():
+            messages.error(self.request, 'Username already taken!')
+
+            return render(self.request, 'users/register.html')
+
+
+class EmailHandler(Handler):
+    def __init__(self, email, all_users, request):
+        super(EmailHandler, self).__init__()
+        self.email = email
+        self.all_users = all_users
+        self.request = request
+
+    def handle(self):
+        if self.all_users.filter(email=self.email).exists():
+            messages.error(self.request, 'Email already taken!')
+
+            return render(self.request, 'users/register.html')
+
+
+class PasswordHandler(Handler):
+    def __init__(self, password, password2, all_users, request):
+        super(PasswordHandler, self).__init__()
+        self.password = password
+        self.password2 = password2
+        self.all_users = all_users
+        self.request = request
+
+    def handle(self):
+        if self.password != self.password2:
+            messages.error(self.request, 'Passwords do not match!')
+
+            return render(self.request, 'users/register.html')
--- users/views.py
+++ users/views.py
@@ -1,12 +1,12 @@
 from django.contrib import messages, auth
 from django.shortcuts import redirect, render
-from django.contrib.auth.models import User
-from grades.models import Grade
 
-from users.models import Person, Student, Professor
+from grades.models import Grade
+from invitations.models import Invitation
 from notifications.models import Notification
 from notifications.views import create_notification
-from invitations.models import Invitation
+from users.models import Person, Student, Professor
+from users.user_builder import UserBuilder
 
 
 def register(request):
@@ -16,93 +16,13 @@ def register(request):
     if request.method != 'POST':
         return render(request, 'users/register.html')
 
-    register_user(request)
+    user_builder = UserBuilder(request)
 
-    register_person(request)
-
-    is_student = request.POST['is_student']
-
-    if is_student == 'True':
-        register_student(request)
-    else:
-        register_professor(request)
+    user_builder.register_account()
 
     messages.success(request, 'Successfully registered! You can now log in.')
-    
+
     return redirect('login')
-
-
-def register_user(request):
-    first_name = request.POST['first_name'].capitalize()
-    last_name = request.POST['last_name'].capitalize()
-    username = request.POST['username']
-    email = request.POST['email']
-    password = request.POST['password']
-    password2 = request.POST['password2']
-
-    allUsers = User.objects.all()
-
-    if allUsers.filter(username=username).exists():
-        messages.error(request, 'Username already taken!')
-        
-        return render(request, 'users/register.html')
-
-    if allUsers.filter(email=email).exists():
-        messages.error(request, 'Email already taken!')
-        
-        return render(request, 'users/register.html')
-
-    if password != password2:
-        messages.error(request, 'Passwords do not match!')
-        
-        return render(request, 'users/register.html')
-
-    user = User.objects.create_user(first_name=first_name, last_name=last_name,
-                                    username=username, email=email)
-    user.set_password(password)
-
-    user.save()
-
-
-def register_person(request):
-    user = User.objects.get(username=request.POST['username'])
-    photo = request.FILES['photo']
-    phone = request.POST['phone']
-    country = request.POST['country']
-    address = request.POST['address']
-    birth_date = request.POST['birth_date']
-    gender = request.POST['gender']
-
-    person = Person.objects.create(user=user, photo=photo, phone=phone,
-                                   country=country, address=address, birth_date=birth_date, gender=gender)
-    person.save()
-
-
-def register_student(request):
-    person = Person.objects.get(user=User.objects.get(
-        username=request.POST['username']))
-    identification_no = request.POST['identification_no']
-    university = request.POST['university']
-    faculty = request.POST['faculty']
-    study_level = request.POST['study_level']
-    study_year = request.POST['study_year']
-
-    student = Student.objects.create(person=person, identification_no=identification_no,
-                                     university=university, faculty=faculty, study_level=study_level, study_year=study_year)
-    student.save()
-
-
-def register_professor(request):
-    person = Person.objects.get(user=User.objects.get(
-        username=request.POST['username']))
-    department = request.POST['department']
-    rank = request.POST['rank']
-    office_address = request.POST['office_address']
-    website = request.POST['website']
-
-    professor = Professor.objects.create(
-        person=person, department=department, rank=rank, office_address=office_address, website=website)
-    professor.save()
 
 
 def login(request):
@@ -118,7 +38,6 @@ def login(request):
     user = auth.authenticate(username=username, password=password)
 
     if user is None:
-
         messages.error(request, 'Invalid credentials!')
         return redirect('login')
 
@@ -132,9 +51,9 @@ def logout(request):
         return redirect('index')
 
     auth.logout(request)
-    
+
     messages.success(request, 'You are now logged out.')
-    
+
     return redirect('login')
 
 
@@ -154,7 +73,7 @@ def dashboard(request):
         is_student = False
 
     try:
-        invitations = Invitation.objects.filter(to_user=request.user).filter(closed = False)
+        invitations = Invitation.objects.filter(to_user=request.user).filter(closed=False)
     except:
         invitations = None
 
@@ -183,7 +102,7 @@ def profile(request):
         is_student = False
         student = None
         grades = None
-        
+
     context = {
         'is_student': is_student,
         'person': person,
